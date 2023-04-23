8a715951472c7cc327c7f3a769d6749991dd0c82
PrandyZw <1322850251@qq.com>
2023-04-23 18:06:33 +0800

    初始化项目

---
+++ src/main/java/com/yzw/advance/AdvanceCodeApplication.java
@@ -0,0 +1,13 @@
+package com.yzw.advance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvanceCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yzw/advance/SpringbootAdvanceCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzw.advance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdvanceCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
